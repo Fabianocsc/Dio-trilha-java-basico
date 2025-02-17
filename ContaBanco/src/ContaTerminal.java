8d74f178cc751e61430e423256b0ae72bdc5abd6
Fabianocsc <fabiano.cesar000@gmail.com>
2025-02-17 12:14:10 -0300

    Update ContaTerminal.java

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,25 +1,25 @@
 import java.util.Locale;
-import java.util.Scanner;  // Corrigido para a classe Scanner com "S" maiúsculo
+import java.util.Scanner; 
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
 
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US); // A forma de inicialização está correta, mas cuidado com o uso de Locale
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         System.out.println("Por favor, digite o número da conta");
-        int numeroConta = scanner.nextInt(); // Leitura do número da conta
+        int numeroConta = scanner.nextInt();
 
         System.out.println("Agora digite a agência");
-        String agencia = scanner.next(); // Leitura da agência
+        String agencia = scanner.next(); 
 
         System.out.println("Digite seu nome");
-        String nome = scanner.next(); // Leitura do nome
+        String nome = scanner.next();
 
-        Double saldo = 1500.0; // Saldo inicial
+        Double saldo = 1500.0;
 
-        // Corrigindo a saída com espaços apropriados
+        
         System.out.println("Olá " + nome + ", obrigado por criar uma conta com nosso banco. Sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo de R$ " + saldo + " já está disponível para saque.");
 
-        scanner.close(); // Fechar o scanner após o uso
+        scanner.close(); 
     }
 }
